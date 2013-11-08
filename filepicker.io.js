b61ebbed282d3615c70fed47375c655d5207d9e6
rickdog <rickmp3s@gmail.com>
2013-11-08 06:39:09 -0600

    Create filepicker.io.js

---
+++ filepicker.io.js
@@ -0,0 +1,54 @@
+<script>
+(
+  function()
+	 {
+		 var start = function() { showButton(); };
+		 if (typeof filepicker=='undefined')
+		 {
+			 var s = document.createElement('script');
+			 s.src = 'http://api.filepicker.io/v1/filepicker.js';
+			 if (s.addEventListener)
+			 {
+				 s.addEventListener("load", start, false);
+			 }
+			 else
+			 {
+				s.onreadystatechange=function()
+				{
+					if (this.readyState=="complete")
+					{
+						start();
+						s = null;
+					}
+				}
+			 }
+			 s.type = "text/javascript";
+			 document.getElementsByTagName('head')[0].appendChild(s);
+		 }
+		 else
+		 {
+			 start();
+		 }
+	 }
+)();
+
+function showButton()
+{
+	
+	doDialog();
+}
+
+function doDialog()
+{
+		filepicker.setKey('A0b1GBdwgSyv6Pop9ZFNQz');
+
+		// load dialog
+		filepicker.pick
+		(
+			function(InkBlob)
+			{
+	  			console.log(InkBlob.url);
+			}
+		);
+}
+</script>
