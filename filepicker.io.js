67f73dd6700a758c90b688e4fd8f386178c16d63
rickdog <rickmp3s@gmail.com>
2015-03-15 18:05:05 -0500

    Update filepicker.io.js

--- filepicker.io.js
+++ filepicker.io.js
@@ -1,53 +1,49 @@
-(
-  function()
-	 {
-		 var start = function() { showButton(); };
-		 if (typeof filepicker=='undefined')
-		 {
-			 var s = document.createElement('script');
-			 s.src = '//api.filepicker.io/v1/filepicker.js';
-			 if (s.addEventListener)
-			 {
-				 s.addEventListener("load", start, false);
-			 }
-			 else
-			 {
-				s.onreadystatechange=function()
-				{
-					if (this.readyState=="complete")
-					{
-						start();
-						s = null;
-					}
-				}
-			 }
-			 s.type = "text/javascript";
-			 document.getElementsByTagName('head')[0].appendChild(s);
-		 }
-		 else
-		 {
-			 start();
-		 }
-	 }
-)();
-
+(function ()
+{
+  var start = function ()
+  {
+    showButton();
+  };
+  if (typeof filepicker == 'undefined')
+  {
+    var s = document.createElement('script');
+    var pro = (document.location.protocol == 'https:' ? 'https:' : 'http:');
+    s.src = pro + '//api.filepicker.io/v1/filepicker.js';
+    if (s.addEventListener)
+    {
+      s.addEventListener('load', start, false);
+    } 
+    else
+    {
+      s.onreadystatechange = function ()
+      {
+        if (this.readyState == 'complete')
+        {
+          start();
+          s = null;
+        }
+      }
+    }
+    s.type = 'text/javascript';
+    document.getElementsByTagName('head') [0].appendChild(s);
+  } 
+  else
+  {
+    start();
+  }
+}
+) ();
 function showButton()
 {
-	
-	doDialog();
+  doDialog();
 }
-
 function doDialog()
 {
-		filepicker.setKey('A0b1GBdwgSyv6Pop9ZFNQz');
-
-		// load dialog
-		filepicker.pick
-		(
-			function(InkBlob)
-			{
-	  			console.log(InkBlob.url);
-			}
-		);
+  filepicker.setKey('A0b1GBdwgSyv6Pop9ZFNQz');
+  // load dialog
+  filepicker.pick(function (InkBlob)
+  {
+    console.log(InkBlob.url);
+  }
+  );
 }
-
